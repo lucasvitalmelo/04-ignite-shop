89c6982c921ed4b21ba2b96ebeca9acfa802cd66
Lucas Vital de Melo <lucasvitaldemelo@gmail.com>
2023-05-30 02:09:24 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  background: 'none',
+  color: 'white'
+
+})
+
+export default function Home() {
+  return (
+
+    <h1>Home</h1>
+  )
+}
