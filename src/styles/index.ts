3b8d76ebb9dad20665bb40c88f0de24a253e57cb
lucasvitalmelo <lucasvitaldemelo@gmail.com>
2023-07-27 20:10:13 -0300

    part of classes completed

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -19,8 +19,14 @@ export const {
       gray300: '#c4c4cc',
       gray100: '#e1e1e6',
 
-      green500: '00875f',
-      green300: '00b37e',
+      green500: '#00875f',
+      green300: '#00b37e',
+    },
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem'
     }
   }
 })
